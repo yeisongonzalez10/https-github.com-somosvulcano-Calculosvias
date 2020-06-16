f3261e06645a66a35fdae0d3ad702f436ba5f866
yadiraeleonc <65635776+yadiraeleonc@users.noreply.github.com>
2020-06-15 19:55:04 -0500

    Update MainActivity.kt

--- holaMundo/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ holaMundo/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,6 +12,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // aporte 1 inicio
+        
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.math.truncate
+
+
+class MainActivity : AppCompatActivity() {
+
+    var etC: EditText? = null
+    var etRa: EditText? = null
+    var tvT: TextView? = null
+    var tvCl: TextView? = null
+    var tvE: TextView? = null
+    var tvO: TextView? = null
+    var tvG: TextView? = null
+    var tvL: TextView? = null
+    var tvCu: TextView? = null
+    var tvDm: TextView? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var etDG = findViewById<EditText>(R.id.etDG)
+        var etDM = findViewById<EditText>(R.id.etDM)
+        var etDS = findViewById<EditText>(R.id.etDS)
+        var etC = findViewById<EditText>(R.id.etC)
+        var etRa = findViewById<EditText>(R.id.etRa)
+
+    //aporte 1 fin
+        
+        
+        
+
+
+
+        
 
         }
     }
