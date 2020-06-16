8b1390abedcfba8b3125210390ed13ade95b75c1
yadiraeleonc <65635776+yadiraeleonc@users.noreply.github.com>
2020-06-15 20:16:11 -0500

    Update MainActivity.kt

--- holaMundo/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ holaMundo/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // aporte 1 inicio
+       // aporte 1 inicio
         
 import android.content.Intent
 import android.os.Bundle
